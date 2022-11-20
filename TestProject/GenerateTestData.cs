34543e0d577640fc91c33b6f2435ca65662cc56a
yhan <hanyi8110@gmail.com>
2022-11-20 18:38:16 +0100

    Copy data from one table to another

--- TestProject/GenerateTestData.cs
+++ TestProject/GenerateTestData.cs
@@ -1,7 +1,10 @@
-﻿using System.Globalization;
+﻿using System.Diagnostics;
+using System.Globalization;
 using Common;
 using CsvHelper;
 using CsvHelper.Configuration;
+using EFCore.BulkExtensions;
+using Microsoft.EntityFrameworkCore;
 
 namespace TestProject;
 
@@ -14,7 +17,7 @@ public class GenerateTestData
         config.Add("dataSize", 1_000_000);
         
         var generator = new Generator(config);
-        var marketOrderVms = generator.Execute();
+        List<MarketOrderVm> marketOrderVms = generator.Execute();
         using (var writer = new StreamWriter(@"mkt-time.csv"))
         using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
         {
@@ -27,6 +30,52 @@ public class GenerateTestData
     public void ShowESTimestamp()
     {
         TestContext.WriteLine(DateTimeOffset.UtcNow.ToString("yyyyMMdd'T'hhmmssZ"));
+    }
+
+    [Test]
+    public async Task InsertAll()
+    {
+        var config = new Config();
+        config.Add("dataSize", 1_000_000);
+
+        var generator = new Generator(config);
+        List<MarketOrderVm> marketOrderVms = generator.Execute();
+
+        await using var dbContext = new MarketOrdersContext();
+        var sw = Stopwatch.StartNew();
+        await dbContext.BulkInsertAsync(marketOrderVms);
+        TestContext.WriteLine($"Insert All took {sw.Elapsed}");
+    }
+
+    [Test]
+    public async Task ArchiveWithRawSql()
+    {
+        await using var dbContext = new MarketOrdersContext();
+        var sw = Stopwatch.StartNew();
+        var lines =  await dbContext.Database.ExecuteSqlRawAsync(
+        @"truncate table histo.""MarketOrderVms"";
+          insert into histo.""MarketOrderVms"" select * from daily.""MarketOrderVms"";"); //19s
+
+        TestContext.WriteLine($"Archive {lines} lines took {sw.Elapsed}");
+    }
+
+    [Test]
+    public async Task ArchiveWithFile()
+    {
+        await using var dbContext = new MarketOrdersContext();
+        var sw = Stopwatch.StartNew();
+        dbContext.Database.SetCommandTimeout(TimeSpan.FromMinutes(5));
+        var lines = await dbContext.Database.ExecuteSqlRawAsync(
+        
+        @"truncate table histo.""MarketOrderVms"";
+          
+          -- copy table data to file
+          copy (select * from daily.""MarketOrderVms"") to 'c:\yi\data\MarketOrderVms.copy';
+          
+          -- copy data from file to table
+          copy histo.""MarketOrderVms"" from 'c:\yi\data\MarketOrderVms.copy';");
+
+        TestContext.WriteLine($"Archive {lines} lines took {sw.Elapsed}"); // 00:00:59.0979567
     }
 }
 
---
+++ TestProject/MarketOrdersContext.cs
@@ -0,0 +1,56 @@
+﻿using Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace TestProject;
+
+public class MarketOrdersContext : DbContext
+{
+    public MarketOrdersContext(DbContextOptions<MarketOrdersContext> optionsBuilderOptions)
+        :base(optionsBuilderOptions)
+    {
+    }
+
+    public MarketOrdersContext()
+    {
+    }
+    
+    public DbSet<MarketOrderVm> MarketOrderVms { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=OrdersDB;Username=postgres;Password=postgres;Search Path=daily");
+    }
+    
+    // protected override void OnModelCreating(DbModelBuilder modelBuilder)
+    // {
+    //     modelBuilder.HasDefaultSchema("public");
+    //     base.OnModelCreating(modelBuilder);
+    // }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("daily");
+        modelBuilder
+            .Entity<MarketOrderVm>(
+            eb => {
+                eb.HasNoKey();
+                // eb.ToView("View_BlogPostCounts");
+                // eb.Property(v => v.BlogName).HasColumnName("Name");
+            });
+        base.OnModelCreating(modelBuilder);
+    }
+}
+
+
+public class MarketOrdersContextFactory : IDesignTimeDbContextFactory<MarketOrdersContext>
+{
+    public MarketOrdersContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<MarketOrdersContext>();
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=OrdersDB;Username=postgres;Password=postgres;Search Path=daily",
+        builder => builder.MigrationsHistoryTable("__EFMigrationsHistory", "daily"));
+        
+
+        return new MarketOrdersContext(optionsBuilder.Options);
+    }
+}
